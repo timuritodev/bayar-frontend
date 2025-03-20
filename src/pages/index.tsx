4c3d0d43ce2b335f23ce19da96415070e952d74f
Салахутдинов Тимур Ренатович <tsalakhutdinov@kzn.beeline.ru>
2025-03-20 03:17:50 +0300

    added seo to main-page and added lang=ru 2.0

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,15 @@ import styles from "./index.module.scss";
 const MainPage = () => {
   const dispatch = useAppDispatch();
 
+  const page_title = "Сэндвич панели - BAYAR";
+  const page_description = "Сэндвич-панели по лучшим ценам в РФ";
+  const og_image = "https://tatbayar.ru/api/images/open_graph.jpeg";
+
   return (
     <>
       <Head>
-        <title>Сэндвич-панели - BAYAR</title>
-        <meta name="description" content="Сэндвич-панели по лучшим ценам в РФ" />
+        <title>{page_title}</title>
+        <meta name="description" content={page_description} />
         <meta name="keywords" content="сэндвич-панели, сэндвич панели купить, панели для строительства, строительные материалы, утепленные панели, Челны, Россия" />
         <meta name="robots" content="index, follow" />
         <meta name="googlebot" content="index, follow" />
@@ -21,22 +25,23 @@ const MainPage = () => {
 
         <meta property="og:site_name" content="BAYAR" />
         <meta property="og:locale" content="ru" />
-        <meta property="og:title" content="Сэндвич-панели - BAYAR" />
-        <meta property="og:description" content="Сэндвич-панели по лучшим ценам в РФ" />
+        <meta property="og:title" content={page_title} />
+        <meta property="og:description" content={page_description} />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://tatbayar.ru" />
-        <meta property="og:image" content="https://tatbayar.ru/api/images/open_graph.jpeg" />
+        <meta property="og:image" content={og_image} />
+        <meta property="og:image:secure_url" content={og_image} />
         <meta property="og:image:alt" content="Сэндвич-панели" />
         <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="1100" />
-        <meta property="og:image:height" content="600" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@tiwithblood" />
         <meta name="twitter:creator" content="@tiwithblood" />
-        <meta name="twitter:title" content="Сэндвич-панели - BAYAR" />
-        <meta name="twitter:description" content="Сэндвич-панели по лучшим ценам в РФ" />
-        <meta name="twitter:image" content="https://tatbayar.ru/api/images/open_graph.jpeg" />
+        <meta name="twitter:title" content={page_title} />
+        <meta name="twitter:description" content={page_description} />
+        <meta name="twitter:image" content={og_image} />
       </Head>
       <div className={styles.main}>
         <div className={styles.container}>
