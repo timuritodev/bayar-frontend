ad4d3157dcf13a639eeeaab02af7774fab808147
luciD <sosalnet@yandex.ru>
2025-10-13 23:46:40 +0300

    added yandex metrika

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,22 +6,32 @@ import { PersistGate } from 'redux-persist/integration/react';
 import '../../public/fonts/fonts.css';
 import '../../public/normalize.css';
 import '../globals.scss';
+import { useYandexMetrikaRouter } from '../hooks/useYandexMetrika';
 import { persistor, store } from '../services/redux/store';
 
 function MyApp({ Component, pageProps }: AppProps) { // TODO проверить какие правильный иконки сюда нужно ставить, в public/icons есть файл site.webmanifest. может быть лучше подключить его?
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Layout>
-          <Head>
-            <link rel="icon" href="/icons/favicon.ico" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-            <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
-          </Head>
-          <Component {...pageProps} />
-        </Layout>
+        <AppContent Component={Component} pageProps={pageProps} />
       </PersistGate>
     </Provider>
+  );
+}
+
+function AppContent({ Component, pageProps }: { Component: AppProps['Component']; pageProps: AppProps['pageProps'] }) {
+  // Отслеживаем переходы между страницами
+  useYandexMetrikaRouter();
+
+  return (
+    <Layout>
+      <Head>
+        <link rel="icon" href="/icons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
