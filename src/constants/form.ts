815c252b3d9ca0a15a0caf1c2ac129efed85b54a
luciD <sosalnet@yandex.ru>
2025-10-19 14:26:12 +0300

    resolve merge conflict in _document.tsx

---
+++ src/constants/form.ts
@@ -0,0 +1,20 @@
+export const options_purpose_panels = [
+	{ value: "стеновые", label: "стеновые" },
+	{ value: "кровельные", label: "кровельные" },
+	{ value: "стеновые и кровельные", label: "стеновые и кровельные" },
+];
+
+export const options_connection_type = [
+	{ value: "whatsapp", label: "Whatsapp" },
+	{ value: "e-mail", label: "E-mail" },
+	{ value: "телефон", label: "Телефон" },
+	{ value: "telegram", label: "Telegram" },
+];
+
+export const options_panel_thickness = [
+	{ value: "80мм", label: "80 мм" },
+	{ value: "100мм", label: "100 мм" },
+	{ value: "120мм", label: "120 мм" },
+	{ value: "150мм", label: "150 мм" },
+	{ value: "200мм", label: "200 мм" },
+];
